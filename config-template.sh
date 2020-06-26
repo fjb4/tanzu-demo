006ef3237545de4e6206b49e1f220f8375ad8a7c
John Bush <fbush@vmware.com>
2020-06-26 15:08:09 -0500

    first attempt at moving to a jupyter notebook

---
+++ config-template.sh
@@ -0,0 +1,27 @@
+# Cluster names
+export CICD_CLUSTER=  # cicd cluster name
+export ALPHA_CLUSTER= # alpha cluster name
+export BRAVO_CLUSTER= # bravo cluster name
+
+# Tanzu Mission Control
+export ACCOUNT_NAME= # tmc account name
+export SSH_KEY_NAME= # tmc ssh key name
+export REGION= # tmc region
+export GROUP= # tmc cluster group
+
+# GitHub
+export GITHUB_USERNAME= # GitHub username
+export GITHUB_PASSWORD= # GitHub password
+
+# Docker Hub
+export DOCKERHUB_USERNAME= # Docker Hub username
+export DOCKERHUB_PASSWORD= # Docker Hub password
+
+# Alpha cluster settings
+export ALPHA_NAMESPACE= # Alpha cluster namespace
+export ALPHA_RABBITMQ_PASSWORD= # RabbitMQ password
+export ALPHA_REDIS_PASSWORD= # Redis password
+export ALPHA_WAVEFRONT_TOKEN= # Wavefront token
+
+# Bravo cluster settings
+export BRAVO_NAMESPACE= # Bravo cluster namespace
